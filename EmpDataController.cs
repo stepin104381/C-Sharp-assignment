b513aaa8dce3fdf0889be640c10204a3887df54b
nisargahs <57631871+stepin104381@users.noreply.github.com>
2020-10-02 18:43:40 -0700

    Add files via upload

---
+++ EmpDataController.cs
@@ -0,0 +1,66 @@
+﻿using SampleMvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvcApp.Controllers
+{
+    public class EmpDataController : Controller
+    {
+        public ViewResult AllEmployees()
+        {
+            var context = new MyDatabase();
+            var model = context.EmpTables.ToList();
+            return View(model);
+        }
+
+        public ViewResult Find(string id)
+        {
+            int empId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == empId);
+            return View(model);
+
+        }
+     
+        [HttpPost]
+        public ActionResult Find(EmpTable emp)
+        {
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == emp.EmpID);
+            model.EmpName = emp.EmpName;
+            model.EmpAddress = emp.EmpAddress;
+            model.EmpSalary = emp.EmpSalary;
+            context.SaveChanges();
+            return RedirectToAction("AllEmployees");
+        }
+
+        public ViewResult NewEmployee()
+        {
+            var model = new EmpTable();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult NewEmployee(EmpTable emp)
+        {
+            var context = new MyDatabase();
+            context.EmpTables.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("AllEmployees");
+        }
+
+        public ActionResult Delete(string id)
+        {
+           
+            int empId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == empId);
+            context.EmpTables.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("AllEmployees");
+        }
+    }
+}
